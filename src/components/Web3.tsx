78ec1994ec22c06ca721d1b0e8c997f3c56b7e38
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-10-26 21:18:00 +0100

    notes on ConnectWallet

--- src/components/Web3.tsx
+++ src/components/Web3.tsx
@@ -1,21 +1,32 @@
 import { createConfig, http } from "wagmi";
 import { mainnet } from "wagmi/chains";
-import { WagmiWeb3ConfigProvider } from "@ant-design/web3-wagmi";
-import { Address, NFTCard } from "@ant-design/web3";
+ import { WagmiWeb3ConfigProvider, MetaMask } from "@ant-design/web3-wagmi";
+ import { Address, NFTCard, Connector, ConnectButton } from "@ant-design/web3";
+ import { injected } from "wagmi/connectors";
 
 const config = createConfig({
-chains: [mainnet],
-transports: {
-[mainnet.id]: http('https://api.zan.top/node/v1/eth/mainnet/62a6e4c05a1b430db908dd482867f497'), 
-    },
+  chains: [mainnet],
+  transports: {
+    [mainnet.id]: http(),
+  },
+   connectors: [
+     injected({
+       target: "metaMask",
+     }),
+   ],
 });
 
 export default function Web3() {
-  
   return (
-    <WagmiWeb3ConfigProvider config={config}>
-        <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
-        <NFTCard address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" tokenId={641} />
+    <WagmiWeb3ConfigProvider config={config} wallets={[MetaMask()]}>
+      <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
+      <NFTCard
+        address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9"
+        tokenId={641}
+      />
+       <Connector>
+         <ConnectButton />
+     </Connector>
     </WagmiWeb3ConfigProvider>
   );
 };
