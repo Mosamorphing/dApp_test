aecd19129ee48ade69d6bd7545c3688b3515f1f0
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-10-28 09:12:20 +0100

    i had a discrepancy due to case sensitive naming. i had to delete it and create a new file altogether to resolve it

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Web3 from './components/Web3.tsx';
+import WebTemp from './components/WebTemp';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<h1>Welcome to my dApp testing ground</h1>} />
-        <Route path="/web3" element={<Web3 />} />
+        <Route path="/web3" element={<WebTemp />} />
       </Routes>
     </Router>
   );
